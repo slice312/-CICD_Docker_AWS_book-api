cdae71839e73d2c73c4ac235fbcb2aed08614113
Talgat Tairov <tairovtalgat@gmail.com>
2022-12-21 23:58:02 +0600

    CICD

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
