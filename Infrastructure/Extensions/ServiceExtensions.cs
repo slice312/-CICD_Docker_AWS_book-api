b992be3fa1c96f8fd52a5c232894cb94751b14b3
Talgat Tairov <tairovtalgat@gmail.com>
2023-01-05 02:03:32 +0600

    test

--- Infrastructure/Extensions/ServiceExtensions.cs
+++ Infrastructure/Extensions/ServiceExtensions.cs
@@ -12,6 +12,7 @@ public static class ServiceExtensions
             Environment.GetEnvironmentVariable("FRONTEND_ALLOWED_HOSTS")
                 ?.Split(";", StringSplitOptions.RemoveEmptyEntries);
 
+        Console.WriteLine(string.Join(", ", allowedCorsHosts));
         if (allowedCorsHosts is not null)
         {
             services.AddCors(options =>
